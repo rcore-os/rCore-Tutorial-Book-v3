1ed5751a73f8f57345505791870fba1114a80146
Yifan Wu <shinbokuow@163.com>
2020-11-16 10:13:10 +0800

    Try Utterance after installing sphinx-comments.

--- source/conf.py
+++ source/conf.py
@@ -33,6 +33,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_comments"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
