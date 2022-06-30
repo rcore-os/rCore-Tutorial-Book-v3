3985ed8c87b83b9071855da55c9564517669028a
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2022-06-30 12:44:23 +0800

    update conf.py

--- source/conf.py
+++ source/conf.py
@@ -60,8 +60,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
